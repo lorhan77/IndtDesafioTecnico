15116f150aad7a6daca5cded2c2b173300c8bc40
lorhan77 <gabriellorhan7@gmail.com>
2023-12-31 19:37:43 -0400

    Api Cors

--- IndtApi/Program.cs
+++ IndtApi/Program.cs
@@ -1,7 +1,18 @@
 using IndtApi.Data;
 using Microsoft.EntityFrameworkCore;
 
+var MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(name: MyAllowSpecificOrigins,
+                      policy =>
+                      {
+                          policy.WithOrigins("http://localhost:3000");
+                      });
+});
 
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
 {
@@ -14,6 +25,8 @@ builder.Services.AddControllers();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+
+app.UseCors(MyAllowSpecificOrigins);
 
 app.UseHttpsRedirection();
 
